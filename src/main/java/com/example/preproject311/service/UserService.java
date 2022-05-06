2101acfef5ce8a80f5c15ac09c71a8fb3f8604e7
Dmitrii Nemtsov <dimas224@yandex.ru>
2022-05-06 16:38:56 +0300

    Добавлен @Transactional

--- src/main/java/com/example/preproject311/service/UserService.java
+++ src/main/java/com/example/preproject311/service/UserService.java
@@ -5,18 +5,13 @@ import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface UserService {
-    @Transactional
     List<User> findAll();
 
-    @Transactional
     User findById(long id);
 
-    @Transactional
     void save(User user);
 
-    @Transactional
     void update(long id, User updatedUser);
 
-    @Transactional
     void delete(long id);
 }
--- src/main/java/com/example/preproject311/service/UserServiceImpl.java
+++ src/main/java/com/example/preproject311/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.preproject311.repo.UserRepo;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -13,16 +14,19 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepo userRepo;
 
+    @Transactional(readOnly = true)
     @Override
     public List<User> findAll() {
         return userRepo.findAll();
     }
 
+    @Transactional(readOnly = true)
     @Override
     public User findById(long id) {
         return userRepo.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
     }
 
+    @Transactional
     @Override
     public void save(User user) {
         userRepo.save(user);
@@ -36,6 +40,7 @@ public class UserServiceImpl implements UserService {
         userRepo.save(user);
     }
 
+    @Transactional
     @Override
     public void delete(long id) {
         userRepo.delete(findById(id));
